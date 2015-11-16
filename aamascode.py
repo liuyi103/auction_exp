d146f61fd6f32d51dfbc02a0ec4d656e51966192
Yicheng Liu <61c@live.cn>
2015-11-17 00:15:06 +0800

    new code

--- aamascode.py
+++ aamascode.py
@@ -23,24 +23,52 @@ def getRevenue(instance, levels):
     cnt_l2 = len(instance) - 1 - find(instance, levels[l2-1])
     return (cnt_l2 * levels[l2-1] + levels[l2]) / (cnt_l2 + 1.0)
 
-num_instance = 20
+# num_instance = 20
+#
+# '''
+# In the following code, we want to select 5 bid levels from level_base
+# '''
+# level_base = np.r_[0:1:0.01]
+# print level_base
+# for m in range(10, 110, 10):
+#     instances = [genInstance(m) for i in range(num_instance)]
+#     dp = {(0, 1):[0]}
+#     for k, level in enumerate(level_base):
+#         if k == 0:
+#             continue
+#         for n_level in range(1, 6):
+#             if (k-1, n_level-1) not in dp:
+#                 break
+#             dp[k][n_level]
+import math
 
-'''
-In the following code, we want to select 5 bid levels from level_base
-'''
-level_base = np.r_[0:1:0.01]
-print level_base
-for m in range(10, 110, 10):
-    instances = [genInstance(m) for i in range(num_instance)]
-    dp = {(0, 1):[0]}
-    for k, level in enumerate(level_base):
-        if k == 0:
-            continue
-        for n_level in range(1, 6):
-            if (k-1, n_level-1) not in dp:
-                break
-            dp[k][n_level]
+def getr(lastlevel, level, n):
+    '''
 
+    :param lastlevel:
+    :param level:
+    :param n: the number of bidders
+    :return: the revenue
+    '''
+    # case 1, if there are more than one between the two levels.
+    p1 = 0
+    for k in range(2, n+1):
+        p1 += lastlevel*(1-math.exp(-lastlevel))**(n-k)*(math.exp(-lastlevel)-math.exp(-level))**k*math.factorial(n)/math.factorial(k)/math.factorial(n-k)
+    # case 2, the top one is above level
+    p2 = 0
+    for k in range(1,n):
+        p2 += math.factorial(n)/math.factorial(k)/math.factorial(n-1-k)*(lastlevel+k*level)/(k+1.)*(1-math.exp(-lastlevel))**(n-k-1)*(math.exp(-lastlevel)-math.exp(-level))**k*math.exp(-level)
+    return p1+p2
+for n in range(10, 110, 10):
+    factor = -math.log(1./n, math.e)*3
+    dp = np.zeros((101, 6))
+    for i in range(1, 101):
+        level = i * 0.01 * factor
+        for k in range(6):  # k is the number of bid levels used.
+            for j in range(i):
+                lastlevel = j * 0.01 * factor
+                dp[i][k] = max(dp[i][k], dp[j][k-1] + getr(lastlevel, level, n))
+    print dp[-1,-1]
 # for m in range(10,110,10):
 #     print m
 #     for kk in range(20):
